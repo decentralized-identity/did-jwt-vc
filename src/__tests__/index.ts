874abbb91b29e8187f8c56708ae64f1b7be2281b
mi-xu <mxu11290@gmail.com>
2019-08-01 12:29:18 -0400

    Verifiable Credential and Presentation attribute validation (#2)
    feat: added runtime validation of attributes when creating a Verifiable Credential or Presentation

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -2,70 +2,238 @@ import EthrDID from 'ethr-did'
 import { createVerifiableCredential, createPresentation } from '../index'
 import { decodeJWT } from 'did-jwt'
 
+const INVALID_DID = 'this is not a valid did'
+const INVALID_TIMESTAMP = 1563905309015
 const issuerIdentity = {
   did: 'did:ethr:0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
   address: '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
   privateKey: 'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75'
 }
-
 const did = new EthrDID(issuerIdentity)
-
+const verifiableCredentialPayload = {
+  sub: 'did:ethr:0x435df3eda57154cf8cf7926079881f2912f54db4',
+  nbf: 1562950282,
+  vc: {
+    '@context': [
+      'https://www.w3.org/2018/credentials/v1',
+      'https://www.w3.org/2018/credentials/examples/v1'
+    ],
+    type: ['VerifiableCredential', 'UniversityDegreeCredential'],
+    credentialSubject: {
+      degree: {
+        type: 'BachelorDegree',
+        name: 'Baccalauréat en musiques numériques'
+      }
+    }
+  }
+}
 const exampleVcJwt =
-  `eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NjM4MjQ4MDksInN1YiI6ImRp
-  ZDpldGhyOjB4MTIzNDU2NzgiLCJuYmYiOjE1NjI5NTAyODI4MDEsInZjIjp7IkBjb250ZXh0IjpbIm
-  h0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIiwiaHR0cHM6Ly93d3cudzMub3Jn
-  LzIwMTgvY3JlZGVudGlhbHMvZXhhbXBsZXMvdjEiXSwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudG
-  lhbCIsIlVuaXZlcnNpdHlEZWdyZWVDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImRl
-  Z3JlZSI6eyJ0eXBlIjoiQmFjaGVsb3JEZWdyZWUiLCJuYW1lIjoiQmFjY2FsYXVyw6lhdCBlbiBtdX
-  NpcXVlcyBudW3DqXJpcXVlcyJ9fX0sImlzcyI6ImRpZDpldGhyOjB4ZjEyMzJmODQwZjNhZDdkMjNm
-  Y2RhYTg0ZDZjNjZkYWMyNGVmYjE5OCJ9.uYSRgDNmZnz0k5rORCBIIzEahVask5eQ2PFZI2_JAatvr
-  pZ2t_3iTvPmBy6Kzt2W20fw5jUJ7GoZXJqoba4UVQA`
+  // tslint:disable-next-line: max-line-length
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1NjM4MjQ4MDksInN1YiI6ImRpZDpldGhyOjB4MTIzNDU2NzgiLCJuYmYiOjE1NjI5NTAyODI4MDEsInZjIjp7IkBjb250ZXh0IjpbImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIiwiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvZXhhbXBsZXMvdjEiXSwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIlVuaXZlcnNpdHlEZWdyZWVDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7ImRlZ3JlZSI6eyJ0eXBlIjoiQmFjaGVsb3JEZWdyZWUiLCJuYW1lIjoiQmFjY2FsYXVyw6lhdCBlbiBtdXNpcXVlcyBudW3DqXJpcXVlcyJ9fX0sImlzcyI6ImRpZDpldGhyOjB4ZjEyMzJmODQwZjNhZDdkMjNmY2RhYTg0ZDZjNjZkYWMyNGVmYjE5OCJ9.uYSRgDNmZnz0k5rORCBIIzEahVask5eQ2PFZI2_JAatvrpZ2t_3iTvPmBy6Kzt2W20fw5jUJ7GoZXJqoba4UVQA'
+const presentationPayload = {
+  vp: {
+    '@context': [
+      'https://www.w3.org/2018/credentials/v1',
+      'https://www.w3.org/2018/credentials/examples/v1'
+    ],
+    type: ['VerifiableCredential'],
+    verifiableCredential: [exampleVcJwt]
+  }
+}
 
 describe('createVerifiableCredential', () => {
   it('creates a valid Verifiable Credential JWT with required fields', async () => {
     const vcJwt = await createVerifiableCredential(
-      {
-        sub: 'did:ethr:0x435df3eda57154cf8cf7926079881f2912f54db4',
-        nbf: 1562950282,
-        vc: {
-          '@context': [
-            'https://www.w3.org/2018/credentials/v1',
-            'https://www.w3.org/2018/credentials/examples/v1'
-          ],
-          type: ['VerifiableCredential', 'UniversityDegreeCredential'],
-          credentialSubject: {
-            degree: {
-              type: 'BachelorDegree',
-              name: 'Baccalauréat en musiques numériques'
-            }
-          }
-        }
-      },
+      verifiableCredentialPayload,
       did
     )
     const decodedVc = await decodeJWT(vcJwt)
     const { iat, ...payload } = decodedVc.payload
     expect(payload).toMatchSnapshot()
   })
+  it('throws a TypeError if sub is not a valid did', async () => {
+    await expect(
+      createVerifiableCredential(
+        {
+          ...verifiableCredentialPayload,
+          sub: INVALID_DID
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if nbf is not a valid timestamp in seconds', async () => {
+    await expect(
+      createVerifiableCredential(
+        {
+          ...verifiableCredentialPayload,
+          nbf: INVALID_TIMESTAMP
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if it does not contain at least the default @context', async () => {
+    await expect(
+      createVerifiableCredential(
+        {
+          ...verifiableCredentialPayload,
+          vc: {
+            '@context': [],
+            type: verifiableCredentialPayload.vc.type,
+            credentialSubject: verifiableCredentialPayload.vc.credentialSubject
+          }
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if it does not contain at least the default type', async () => {
+    await expect(
+      createVerifiableCredential(
+        {
+          ...verifiableCredentialPayload,
+          vc: {
+            '@context': verifiableCredentialPayload.vc['@context'],
+            type: [],
+            credentialSubject: verifiableCredentialPayload.vc.credentialSubject
+          }
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if the credentialSubject is empty', async () => {
+    await expect(
+      createVerifiableCredential(
+        {
+          ...verifiableCredentialPayload,
+          vc: {
+            '@context': verifiableCredentialPayload.vc['@context'],
+            type: verifiableCredentialPayload.vc.type,
+            credentialSubject: {}
+          }
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if exp is present and is not a valid timestamp in seconds', async () => {
+    await expect(
+      createVerifiableCredential(
+        {
+          ...verifiableCredentialPayload,
+          exp: INVALID_TIMESTAMP
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if iss is not a valid did', async () => {
+    await expect(
+      createVerifiableCredential(verifiableCredentialPayload, {
+        did: INVALID_DID,
+        signer: did.signer
+      })
+    ).rejects.toThrow(TypeError)
+  })
 })
 
 describe('createPresentation', () => {
   it('creates a valid Presentation JWT with required fields', async () => {
-    const presentationJwt = await createPresentation(
-      {
-        vp: {
-          '@context': [
-            'https://www.w3.org/2018/credentials/v1',
-            'https://www.w3.org/2018/credentials/examples/v1'
-          ],
-          type: ['VerifiableCredential'],
-          verifiableCredential: [exampleVcJwt]
-        }
-      },
-      did
-    )
+    const presentationJwt = await createPresentation(presentationPayload, did)
     const decodedPresentation = await decodeJWT(presentationJwt)
     const { iat, ...payload } = decodedPresentation.payload
     expect(payload).toMatchSnapshot()
+  })
+  it('throws a TypeError if vp does not contain at least the default @context', async () => {
+    await expect(
+      createPresentation(
+        {
+          ...presentationPayload,
+          vp: {
+            '@context': [],
+            type: presentationPayload.vp.type,
+            verifiableCredential: presentationPayload.vp.verifiableCredential
+          }
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if vp does not contain at least the default type', async () => {
+    await expect(
+      createPresentation(
+        {
+          ...presentationPayload,
+          vp: {
+            '@context': presentationPayload.vp['@context'],
+            type: [],
+            verifiableCredential: presentationPayload.vp.verifiableCredential
+          }
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if vp.verifiableCredential is empty', async () => {
+    await expect(
+      createPresentation(
+        {
+          ...presentationPayload,
+          vp: {
+            '@context': presentationPayload.vp['@context'],
+            type: presentationPayload.vp.type,
+            verifiableCredential: []
+          }
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if vp.verifiableCredential contains a non-JWT string', async () => {
+    await expect(
+      createPresentation(
+        {
+          ...presentationPayload,
+          vp: {
+            '@context': presentationPayload.vp['@context'],
+            type: presentationPayload.vp.type,
+            verifiableCredential: ['this is not a VC JWT']
+          }
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if aud is present and is not a valid did', async () => {
+    await expect(
+      createPresentation(
+        {
+          ...presentationPayload,
+          aud: INVALID_DID
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if exp is present and is not a valid timestamp in seconds', async () => {
+    await expect(
+      createPresentation(
+        {
+          ...presentationPayload,
+          exp: INVALID_TIMESTAMP
+        },
+        did
+      )
+    ).rejects.toThrow(TypeError)
+  })
+  it('throws a TypeError if iss is not a valid did', async () => {
+    await expect(
+      createPresentation(presentationPayload, {
+        did: INVALID_DID,
+        signer: did.signer
+      })
+    ).rejects.toThrow(TypeError)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { createJWT, Signer } from 'did-jwt'
 
 const JWT_ALG = 'ES256K-R'
+const DID_FORMAT = /^did:([a-zA-Z0-9_]+):([:[a-zA-Z0-9_.-]+)(\/[^#]*)?(#.*)?$/
+const JWT_FORMAT = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/
+const DEFAULT_CONTEXT = 'https://www.w3.org/2018/credentials/v1'
+const DEFAULT_TYPE = 'VerifiableCredential'
 
 interface VC {
   '@context': string[]
@@ -25,9 +29,9 @@ interface VP {
 
 interface PresentationPayload {
   vp: VP
-  aud?:string
+  aud?: string
   exp?: number
-  jti?:string
+  jti?: string
 }
 
 interface Issuer {
@@ -39,6 +43,8 @@ export async function createVerifiableCredential(
   payload: VerifiableCredentialPayload,
   issuer: Issuer
 ): Promise<string> {
+  validateVerifiableCredentialAttributes(payload)
+  validateIssuer(issuer)
   return createJWT(payload, {
     issuer: issuer.did,
     signer: issuer.signer,
@@ -50,9 +56,84 @@ export async function createPresentation(
   payload: PresentationPayload,
   issuer: Issuer
 ): Promise<string> {
+  validatePresentationAttributes(payload)
+  validateIssuer(issuer)
   return createJWT(payload, {
     issuer: issuer.did,
     signer: issuer.signer,
     alg: JWT_ALG
   })
+}
+
+// The main scenario we want to guard against is having a timestamp in milliseconds
+// instead of seconds (ex: from new Date().getTime()).
+// We will check the number of digits and assume that any number with 12 or more
+// digits is a millisecond timestamp.
+// 10 digits max is 9999999999 -> 11/20/2286 @ 5:46pm (UTC)
+// 11 digits max is 99999999999 -> 11/16/5138 @ 9:46am (UTC)
+// 12 digits max is 999999999999 -> 09/27/33658 @ 1:46am (UTC)
+function isTimestampInSeconds(t: number): boolean {
+  return Number.isInteger(t) && t < 100000000000
+}
+
+function validateIssuer(issuer: Issuer): void {
+  if (!issuer.did.match(DID_FORMAT)) {
+    throw new TypeError('issuer.did must be a valid did')
+  }
+}
+
+function validateVerifiableCredentialAttributes(
+  payload: VerifiableCredentialPayload
+): void {
+  if (!payload.sub.match(DID_FORMAT)) {
+    throw new TypeError('sub must be a valid did')
+  }
+  if (!isTimestampInSeconds(payload.nbf)) {
+    throw new TypeError('nbf must be a unix timestamp in seconds')
+  }
+  if (
+    payload.vc['@context'].length < 1 ||
+    !payload.vc['@context'].includes(DEFAULT_CONTEXT)
+  ) {
+    throw new TypeError(
+      `vc['@context'] must include at least "${DEFAULT_CONTEXT}"`
+    )
+  }
+  if (payload.vc.type.length < 1 || !payload.vc.type.includes(DEFAULT_TYPE)) {
+    throw new TypeError(`vc.type must include at least "${DEFAULT_TYPE}"`)
+  }
+  if (Object.keys(payload.vc.credentialSubject).length === 0) {
+    throw new TypeError('vc.credentialSubject must not be empty')
+  }
+  if (payload.exp && !isTimestampInSeconds(payload.exp)) {
+    throw new TypeError('exp must be a unix timestamp in seconds')
+  }
+}
+
+function validatePresentationAttributes(
+  payload: PresentationPayload
+): void {
+  if (payload.vp['@context'].length < 1 ||
+  !payload.vp['@context'].includes(DEFAULT_CONTEXT)) {
+    throw new TypeError(
+      `vp['@context'] must include at least "${DEFAULT_CONTEXT}"`
+    )
+  }
+  if (payload.vp.type.length < 1 || !payload.vp.type.includes(DEFAULT_TYPE)) {
+    throw new TypeError(`vp.type must include at least "${DEFAULT_TYPE}"`)
+  }
+  if (payload.vp.verifiableCredential.length < 1) {
+    throw new TypeError('vp.verifiableCredential must not be empty')
+  }
+  for (const vc of payload.vp.verifiableCredential) {
+    if(!vc.match(JWT_FORMAT)) {
+      throw new TypeError('vp.verifiableCredential must only contain JWTs')
+    }
+  }
+  if (payload.aud && !payload.aud.match(DID_FORMAT)) {
+    throw new TypeError('aud must be a valid did or undefined')
+  }
+  if (payload.exp && !isTimestampInSeconds(payload.exp)) {
+    throw new TypeError('exp must be a unix timestamp in seconds')
+  }
 }
