da583ded5f77405774529e4c5fa6988b9737deb2
Mike Xu <mxu11290@gmail.com>
2019-08-05 18:42:20 -0400

    refactor: missed a file in previous commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { createJWT, Signer } from 'did-jwt'
-
-const JWT_ALG = 'ES256K-R'
-const DID_FORMAT = /^did:([a-zA-Z0-9_]+):([:[a-zA-Z0-9_.-]+)(\/[^#]*)?(#.*)?$/
-const JWT_FORMAT = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/
-const DEFAULT_CONTEXT = 'https://www.w3.org/2018/credentials/v1'
-const DEFAULT_TYPE = 'VerifiableCredential'
+import {
+  JWT_ALG} from './constants'
+import * as validators from './validators'
 
 interface VC {
   '@context': string[]
@@ -36,7 +33,7 @@ interface PresentationPayload {
   [x: string]: any
 }
 
-interface Issuer {
+export interface Issuer {
   did: string
   signer: Signer
 }
@@ -46,7 +43,7 @@ export async function createVerifiableCredential(
   issuer: Issuer
 ): Promise<string> {
   validateVerifiableCredentialAttributes(payload)
-  validateIssuer(issuer)
+  validators.validateDidFormat(issuer.did)
   return createJWT(payload, {
     issuer: issuer.did,
     signer: issuer.signer,
@@ -59,7 +56,7 @@ export async function createPresentation(
   issuer: Issuer
 ): Promise<string> {
   validatePresentationAttributes(payload)
-  validateIssuer(issuer)
+  validators.validateDidFormat(issuer.did)
   return createJWT(payload, {
     issuer: issuer.did,
     signer: issuer.signer,
@@ -67,75 +64,28 @@ export async function createPresentation(
   })
 }
 
-// The main scenario we want to guard against is having a timestamp in milliseconds
-// instead of seconds (ex: from new Date().getTime()).
-// We will check the number of digits and assume that any number with 12 or more
-// digits is a millisecond timestamp.
-// 10 digits max is 9999999999 -> 11/20/2286 @ 5:46pm (UTC)
-// 11 digits max is 99999999999 -> 11/16/5138 @ 9:46am (UTC)
-// 12 digits max is 999999999999 -> 09/27/33658 @ 1:46am (UTC)
-function isTimestampInSeconds(t: number): boolean {
-  return Number.isInteger(t) && t < 100000000000
-}
-
-function validateIssuer(issuer: Issuer): void {
-  if (!issuer.did.match(DID_FORMAT)) {
-    throw new TypeError('issuer.did must be a valid did')
-  }
-}
-
 function validateVerifiableCredentialAttributes(
   payload: VerifiableCredentialPayload
 ): void {
-  if (!payload.sub.match(DID_FORMAT)) {
-    throw new TypeError('sub must be a valid did')
-  }
-  if (!isTimestampInSeconds(payload.nbf)) {
-    throw new TypeError('nbf must be a unix timestamp in seconds')
-  }
-  if (
-    payload.vc['@context'].length < 1 ||
-    !payload.vc['@context'].includes(DEFAULT_CONTEXT)
-  ) {
-    throw new TypeError(
-      `vc['@context'] must include at least "${DEFAULT_CONTEXT}"`
-    )
-  }
-  if (payload.vc.type.length < 1 || !payload.vc.type.includes(DEFAULT_TYPE)) {
-    throw new TypeError(`vc.type must include at least "${DEFAULT_TYPE}"`)
-  }
+  validators.validateDidFormat(payload.sub)
+  validators.validateContext(payload.vc['@context'])
+  validators.validateType(payload.vc.type)
   if (Object.keys(payload.vc.credentialSubject).length === 0) {
     throw new TypeError('vc.credentialSubject must not be empty')
   }
-  if (payload.exp && !isTimestampInSeconds(payload.exp)) {
-    throw new TypeError('exp must be a unix timestamp in seconds')
-  }
+  if(payload.nbf) validators.validateTimestamp(payload.nbf)
+  if(payload.exp) validators.validateTimestamp(payload.exp)
 }
 
-function validatePresentationAttributes(
-  payload: PresentationPayload
-): void {
-  if (payload.vp['@context'].length < 1 ||
-  !payload.vp['@context'].includes(DEFAULT_CONTEXT)) {
-    throw new TypeError(
-      `vp['@context'] must include at least "${DEFAULT_CONTEXT}"`
-    )
-  }
-  if (payload.vp.type.length < 1 || !payload.vp.type.includes(DEFAULT_TYPE)) {
-    throw new TypeError(`vp.type must include at least "${DEFAULT_TYPE}"`)
-  }
+function validatePresentationAttributes(payload: PresentationPayload): void {
+  validators.validateContext(payload.vp['@context'])
+  validators.validateType(payload.vp.type)
   if (payload.vp.verifiableCredential.length < 1) {
     throw new TypeError('vp.verifiableCredential must not be empty')
   }
   for (const vc of payload.vp.verifiableCredential) {
-    if(!vc.match(JWT_FORMAT)) {
-      throw new TypeError('vp.verifiableCredential must only contain JWTs')
-    }
+    validators.validateJwtFormat(vc)
   }
-  if (payload.aud && !payload.aud.match(DID_FORMAT)) {
-    throw new TypeError('aud must be a valid did or undefined')
-  }
-  if (payload.exp && !isTimestampInSeconds(payload.exp)) {
-    throw new TypeError('exp must be a unix timestamp in seconds')
-  }
+  if(payload.aud) validators.validateDidFormat(payload.aud)
+  if(payload.exp) validators.validateTimestamp(payload.exp)
 }
