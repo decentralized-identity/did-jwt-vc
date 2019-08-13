f7757e7193cd8dea60da8789a4213ce683d81baf
mi-xu <mxu11290@gmail.com>
2019-08-13 15:44:24 -0400

    feat: add Verifiable Credential Builder (#5)
    * feat: allow optional attributes in vc and vp payloads
    
    * refactor: extract constants and validators into their own modules
    
    * refactor: missed a file in previous commit
    
    * feat: add VerifiableCredentialBuilder class
    
    * test: remove only from vc build tests
    
    * feat: allow vc builder to set exp directly or with duration from nbf
    
    * refactor: consolidate types into types.ts
    
    * style: minor formatting
    
    * chore: bump version to 0.1.0-dev.0
    
    * chore: move faker to dev dependencies
    
    * refactor: rename 'expires' to 'validUntil'
    
    * test: validFrom takes precedence over expiresIn
    
    * feat: store expiresIn in a variable and use it to calculate exp during build
    
    * feat: try to calculate exp from nbf and expiresIn if validUntil is not set
    
    * test: do not check for calls to validators in vc builder methods
    
    * feat: remove input validators from builder methods

---
+++ src/__tests__/validators.ts
@@ -0,0 +1,102 @@
+import * as validators from '../validators'
+import {
+  DID_A,
+  INVALID_DID,
+  EXTRA_CONTEXT_A,
+  EXTRA_CONTEXT_B,
+  EXTRA_TYPE_A,
+  EXTRA_TYPE_B,
+  VC_JWT
+} from '.'
+import { DEFAULT_CONTEXT, DEFAULT_TYPE } from '../constants'
+
+describe('validators', () => {
+  describe('validateDidFormat', () => {
+    it('does not throw if the value is a valid did format', () => {
+      expect(() => validators.validateDidFormat(DID_A)).not.toThrow()
+    })
+    it('throws a TypeError if the value is not a valid did format', () => {
+      expect(() => validators.validateDidFormat(INVALID_DID)).toThrow(TypeError)
+    })
+  })
+
+  describe('validateTimestamp', () => {
+    it('does not throw if the value is a valid unix timestamp in seconds', () => {
+      expect(() =>
+        validators.validateTimestamp(Math.floor(new Date().getTime() / 1000))
+      ).not.toThrow()
+    })
+    it('throws a TypeError if the value is a millisecond timestamp', () => {
+      expect(() => validators.validateTimestamp(new Date().getTime())).toThrow(
+        TypeError
+      )
+    })
+    it('throws a TypeError if the value is not an integer', () => {
+      expect(() =>
+        validators.validateTimestamp(new Date().getTime() / 1000)
+      ).toThrow(TypeError)
+    })
+  })
+
+  describe('validateContext', () => {
+    it('does not throw if the value contains only the default context', () => {
+      expect(() => validators.validateContext([DEFAULT_CONTEXT])).not.toThrow()
+    })
+    it('does not throw if the value contains the default context and some user-defined ones', () => {
+      expect(() =>
+        validators.validateContext([
+          DEFAULT_CONTEXT,
+          EXTRA_CONTEXT_A,
+          EXTRA_CONTEXT_B
+        ])
+      ).not.toThrow()
+    })
+    it('throws a TypeError the value contains no contexts', () => {
+      expect(() => validators.validateContext([])).toThrow(TypeError)
+    })
+    it('throws a TypeError the value is missing the default context', () => {
+      expect(() =>
+        validators.validateContext([EXTRA_CONTEXT_A, EXTRA_CONTEXT_B])
+      ).toThrow(TypeError)
+    })
+  })
+
+  describe('validateType', () => {
+    it('does not throw if the value contains only the default type', () => {
+      expect(() => validators.validateType([DEFAULT_TYPE])).not.toThrow()
+    })
+    it('does not throw if the value contains the default type and some user-defined ones', () => {
+      expect(() =>
+        validators.validateType([DEFAULT_TYPE, EXTRA_TYPE_A, EXTRA_TYPE_B])
+      ).not.toThrow()
+    })
+    it('throws a TypeError the value contains no types', () => {
+      expect(() => validators.validateType([])).toThrow(TypeError)
+    })
+    it('throws a TypeError the value is missing the default type', () => {
+      expect(() =>
+        validators.validateType([EXTRA_TYPE_A, EXTRA_TYPE_B])
+      ).toThrow(TypeError)
+    })
+  })
+
+  describe('validateJwtFormat', () => {
+    it('does not throw if the value is a valid JWT format', () => {
+      expect(() => validators.validateJwtFormat(VC_JWT)).not.toThrow()
+    })
+    it('throws a TypeError if the value is not a valid JWT format', () => {
+      expect(() => validators.validateJwtFormat('not a jwt')).toThrow(TypeError)
+    })
+  })
+
+  describe('validateCredentialSubject', () => {
+    it('does not throw if the value is an object with at least one attribute', () => {
+      expect(() =>
+        validators.validateCredentialSubject({ name: 'test' })
+      ).not.toThrow()
+    })
+    it('throws a TypeError if the value is an object with no attributes', () => {
+      expect(() => validators.validateCredentialSubject({})).toThrow(TypeError)
+    })
+  })
+})
