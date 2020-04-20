01d4f6c8855e5288a857137c592c0482f69ede9a
Chris Were <chris@mozzler.com.au>
2020-04-20 20:19:52 +0930

    Remove lib

--- lib/constants.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const JWT_ALG = "ES256K-R";
-export declare const DID_FORMAT: RegExp;
-export declare const JWT_FORMAT: RegExp;
-export declare const DEFAULT_CONTEXT = "https://www.w3.org/2018/credentials/v1";
-export declare const DEFAULT_VC_TYPE = "VerifiableCredential";
-export declare const DEFAULT_VP_TYPE = "VerifiablePresentation";
-//# sourceMappingURL=constants.d.ts.map
--- lib/validators.d.ts
+++
@@ -1,8 +0,0 @@
-import { CredentialSubject } from './types';
-export declare function validateJwtFormat(value: string): void;
-export declare function validateTimestamp(value: number): void;
-export declare function validateContext(value: string[]): void;
-export declare function validateVcType(value: string[]): void;
-export declare function validateVpType(value: string[]): void;
-export declare function validateCredentialSubject(value: CredentialSubject): void;
-//# sourceMappingURL=validators.d.ts.map
